008b2bbd8f709b96733466d64a4373c39cd1f323
Corentin De Souza <corentin@fantazio.eu>
2026-07-17 18:42:40 +0200

    [lib] adapt typedtree 5.1
    This is in addition to 4.14 and 5.0.
    - `Texp_assert` now holds an extra location (version-dependent)
    - `Tmod_apply_unit` is added to `module_expr_desc` (version-independent)
    - `class_infos.ci_id_typehash` is not_available since 5.1
    
    The testsuite is updated to reflect the new `Tmod_apply_unit`, and allow
    version-dependent code via cppo.
    In particular, test core_type_desc/ttyp_class.ml does not compile in 5.1
    because deprecated syntax (`#tvariant` as `Ttyp_class`) became officially
    obsolete.

--- check/tests/core_type_desc/ttyp_class.ml
+++ check/tests/core_type_desc/ttyp_class.ml
@@ -4,6 +4,11 @@ type 'a t = #c as 'a
 class ['a, 'b] c_poly = object end
 type ('a, 'b, 'c) t_poly = ('a, 'b) #c_poly as 'c
 
+
+#if OCAML_VERSION < (5, 1, 0)
 (* old syntax *)
 type ('a, 'b) variant = [ `A of 'a | `B of 'b ]
 type ('a, 'b, 'v) t_var = ('a, 'b) #variant as 'v
+#elif OCAML_VERSION >= (5, 1, 0)
+(* The #variant is now a compilation error: "Unbound class type variant" *)
+#endif
---
+++ check/tests/module_expr_desc/tmod_apply_unit.ml
@@ -0,0 +1,5 @@
+(* Same as tmod_apply in OCaml < 5.1 *)
+
+module F () = struct end
+
+module _ = F ()
